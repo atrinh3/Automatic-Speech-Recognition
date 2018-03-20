a3da7a5578f3297dc8a359a3365a697864f784c5
atrinh3 <36494242+atrinh3@users.noreply.github.com>
2018-03-20 14:06:34 -0700

    Update Gamma_Xi.py

--- Gamma_Xi.py
+++ Gamma_Xi.py
@@ -1,4 +1,4 @@
-# This function calculates a Qx1 vector given alpha, beta and time t.
+a# This function calculates a Qx1 vector given alpha, beta and time t.
 # The input alpha and beta matrices are already in log form.
 # Prior to making the gamma calculations, the log form of both the
 # alpha and beta values have to be undone.  At this point, it
@@ -10,8 +10,8 @@
 # t:      Time value where 0 < t < T
 def get_gamma(alpha, beta, t):
     states = len(alpha[0])
-	alpha_t = alpha[t]
-	beta_t = beta[t]
+    alpha_t = alpha[t]
+    beta_t = beta[t]
     sum = 0
     for q in range(0, states):
         a_q = math.exp(alpha_t[q])
@@ -39,16 +39,16 @@ def get_gamma(alpha, beta, t):
 # covariances:  QxN covariance matrix for calculating p(observation)
 def get_xi(alpha, beta, mfcc, t, transition, means, covariances):
     states = len(alpha[0])
-	alpha_t = alpha[t - 1]
-	beta_t = beta[t]
+    alpha_t = alpha[t - 1]
+    beta_t = beta[t]
     mfcc_t = mfcc[t]
-	xi_matrix = [0] * states
-	for q in range(0, states):
-	    beta_q = beta_t[q]
-		beta_q = math.exp(beta_q)
-		local_distortion = observation_probability(mfcc_t, means, covariances)
-	    r_vect = [0] * states
-		for r in range(0, states):
+    xi_matrix = [0] * states
+    for q in range(0, states):
+        beta_q = beta_t[q]
+        beta_q = math.exp(beta_q)
+        local_distortion = observation_probability(mfcc_t, means, covariances)
+        r_vect = [0] * states
+        for r in range(0, states):
             state_transition = transition[r][q]
             alpha_r = alpha_t[r]
             alpha_r = math.exp(alpha_r)
