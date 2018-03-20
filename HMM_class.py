4539b11f8e5e0d16b2b1ebf35a3f032671d137a9
atrinh3 <36494242+atrinh3@users.noreply.github.com>
2018-03-20 12:36:12 -0700

    Update HMM_class.py

--- HMM_class.py
+++ HMM_class.py
@@ -1,31 +1,31 @@
 class HiddenMarkovModel:	
-	def __init__(self, name):
+    def __init__(self, name):
         self.name = name
-		self.mean = [None]
-		self.covariance = [None]
-		self.transition = [None]
-		self.initial = [None]
+        self.mean = [None]
+        self.covariance = [None]
+        self.transition = [None]
+        self.initial = [None]
 	
-	def update_mean(self, mean_matrix):
-	    self.mean = mean_matrix
+    def update_mean(self, mean_matrix):
+        self.mean = mean_matrix
 	
-	def update_covariance(self, covariance_matrix):
-	    self.covariance = covariance_matrix
+    def update_covariance(self, covariance_matrix):
+        self.covariance = covariance_matrix
 	
-	def update_transition(self, transition_matrix):
-	    self.transition = transition_matrix
+    def update_transition(self, transition_matrix):
+        self.transition = transition_matrix
 	
-	def update_initial(self, initial_state)
-	    self.initial = initial_state
+    def update_initial(self, initial_state)
+        self.initial = initial_state
 		
     def get_mean():
-	    return self.mean
+        return self.mean
 		
-	def get_covariance():
-	    return self.covariance
+    def get_covariance():
+        return self.covariance
 		
-	def get_transition():
-	    return self.transition
+    def get_transition():
+        return self.transition
 		
-	def get_initial():
-	    return self.initial
+    def get_initial():
+        return self.initial
