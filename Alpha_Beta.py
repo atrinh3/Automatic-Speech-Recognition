2798a652f44e0aad5f7cbf830688c7b66f83c652
atrinh3 <36494242+atrinh3@users.noreply.github.com>
2018-03-24 17:52:37 -0700

    Update Alpha_Beta.py

--- Alpha_Beta.py
+++ Alpha_Beta.py
@@ -1,47 +1,51 @@
+import math
+import Observation as obs
+
+
 # Calculates the sum of 2 probabilities when the available numbers
 # are the probability values in log form.  Since there is a
 # representation of -inf, if the difference between the numbers
-# being added is greater than the representation of inf, this 
-# function will simply return the larger value without performing 
+# being added is greater than the representation of inf, this
+# function will simply return the larger value without performing
 # the addition.
 # Requires -
 # a:    The log form of probability a
 # b:    The log form of probability b
 # low:  The scalar representation of -inf when a probability = 0
-def log_add(a, b, low)
+def log_add(a, b, low):
     small = min(a, b)
     big = max(a, b)
-    diff = a - b
+    diff = small - big
     if diff < -(math.log(-low)):
         # big >> small
-	    return big
-    c = b + math.log(1 + math.exp(a - b)
+        return big
+    c = b + math.log(1 + math.exp(a - b))
     return c
 
 
 # Customized log call to allow for log(probabilities)
-# Since probabilities will always be >= 0, the only error would 
-# come when the probability = 0 which would cause the log call to 
+# Since probabilities will always be >= 0, the only error would
+# come when the probability = 0 which would cause the log call to
 # return -inf.  In this function, attempting to call log(0) would
 # return a pre-specified number that will represent -inf.
-# Requires - 
+# Requires -
 # number:   The number the function is taking the log of.
 # log_low:  The scalar representation of -inf when 'number' = 0
 def get_log(number, log_low):
     if number == 0:
         return log_low
     return math.log(number)
-	
+
 
 # Calculate alpha matrix using full MFCC matrix & log(probabilities)
-# Requires - 
+# Requires -
 # transition:   QxQ transition matrix
 # covariances:  QxN covariance matrix
 # states:       Q - number of states
 # means:        QxN mean matrix
 # mfcc:         TxN sideways MFCC matrix
 # t:            current time location in 0<t<T
-def get_alpha(transitions, covariances, states, means, mfcc, t):
+def get_alpha(transition, covariances, states, means, mfcc, t):
     if t < 1:
         # When t = 0, the chance of being in the first state is
         # 100% so the whole column is 0 except for the first state
@@ -53,33 +57,34 @@ def get_alpha(transitions, covariances, states, means, mfcc, t):
         initial_alpha[0] = math.log(1)
         return [initial_alpha]
     current_mfcc = mfcc[t]
-    alpha_matrix = get_alpha(transitions, 
-                             covariances, 
-                             states, 
+    alpha_matrix = get_alpha(transition,
+                             covariances,
+                             states,
                              means,
-                             mfcc, 
-                             t-1)
+                             mfcc,
+                             t - 1)
     previous_alpha = alpha_matrix[t - 1]
     build_alpha = [0] * states
     low_log = -10e30
     for q in range(0, states):
-        local_distortion = observation_probability(current_mfcc, 
-                                                   means, 
-                                                   covariances)
-        local_distortion = get_log(local_distortion)
-        sum = log_low
+        mean = means[q]
+        local_distortion = obs.prob_observation(current_mfcc,
+                                                mean,
+                                                covariances)
+        local_distortion = get_log(local_distortion, low_log)
+        sum = low_log
         for r in range(0, states):
-		    state_transition = transition[r][q]
-		    state_transition = get_log(state_transition)
-		    alpha_r = previous_alpha[r]
-		    product = alpha_r + state_transition
-		    sum = log_add(sum, product, log_low)
+            state_transition = transition[r][q]
+            state_transition = get_log(state_transition, low_log)
+            alpha_r = previous_alpha[r]
+            product = alpha_r + state_transition
+            sum = log_add(sum, product, low_log)
         build_alpha[q] = local_distortion + sum
     return alpha_matrix
-	
-	
+
+
 # Calculate beta matrix using full MFCC matrix & log(probabilities)
-# Requires - 
+# Requires -
 # transition:   QxQ transition matrix
 # covariances:  QxN covariance matrix
 # states:       Q - number of states
@@ -88,20 +93,20 @@ def get_alpha(transitions, covariances, states, means, mfcc, t):
 # t:            current time location in 0<t<T
 def get_beta(transition, covariances, states, means, mfcc, t):
     if t > len(mfcc) - 1:
-	    # When t = T-1, the chance of being in the last state is
-	    # 100% so the whole column is 0 except for the last state
-	    # which will be set to 1.  For log probabilities, 0's are
-	    # represented with -10e30 since log(0) = -inf.  Likewise,
-	    # the 1 will be represented with a 0 since log(1) = 0.    
-	    log_low = -10e30
-	    last_beta = [log_low] * states
-	    last_beta[states - 1] = math.log(1)
-	    return [last_beta]
-    beta_matrix = get_beta(transition, 
-                           covariances, 
-                           states, 
-                           means, 
-                           mfcc, 
+        # When t = T-1, the chance of being in the last state is
+        # 100% so the whole column is 0 except for the last state
+        # which will be set to 1.  For log probabilities, 0's are
+        # represented with -10e30 since log(0) = -inf.  Likewise,
+        # the 1 will be represented with a 0 since log(1) = 0.
+        log_low = -10e30
+        last_beta = [log_low] * states
+        last_beta[states - 1] = math.log(1)
+        return [last_beta]
+    beta_matrix = get_beta(transition,
+                           covariances,
+                           states,
+                           means,
+                           mfcc,
                            t + 1)
     next_mfcc = mfcc[t + 1]
     build_beta = [0] * states
@@ -111,11 +116,11 @@ def get_beta(transition, covariances, states, means, mfcc, t):
         sum = log_low
         for r in range(0, states):
             state_transition = transition[q][r]
-            state_transition = get_log(state_transition)
-            next_distortion = observation_probability(next_mfcc, 
-                                                      means, 
-                                                      covariances)
-            next_distortion = get_log(next_distortion)
+            state_transition = get_log(state_transition, log_low)
+            next_distortion = obs.prob_observation(next_mfcc,
+                                                   means,
+                                                   covariances)
+            next_distortion = get_log(next_distortion, log_low)
             beta_r = next_beta[r]
             product = beta_r + next_distortion + state_transition
             sum = log_add(sum, product, log_low)
