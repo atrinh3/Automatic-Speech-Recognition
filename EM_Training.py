de0ca87ebc13123af11a8ae42f86509b0f59410c
atrinh3 <36494242+atrinh3@users.noreply.github.com>
2018-04-11 22:27:19 -0700

    Update EM_Training.py

--- EM_Training.py
+++ EM_Training.py
@@ -1,14 +1,15 @@
 import AlphaBeta
 import Posteriors as post
 import numpy as np
+import scipy.io.wavfile as wv
 
 
 def probability_evidence(alpha):
     time = len(alpha)
     states = len(alpha[0])
     return alpha[time - 1][states - 1]
-    
-    
+
+
 # Takes a set of alpha matrices for all utterances of a phrase to
 # calculate the aggregate probability of evidence.  All alpha
 # values are in log form so the multiplication of probabilities
@@ -133,11 +134,11 @@ def train_transition(alphas, betas, mfccs, mean_guess, covariance_guess,
         alpha = alphas[epoch]
         beta = betas[epoch]
         for t in range(1, mfcc_len):
-            tmp_x = post.get_xi(alpha, beta, t, transition, mean_guess,
-                           covariance_guess)
-            tmp_g = post.get_gamma(alpha, beta, t)
-            xi = np.add(xi, tmp_x)
-            gamma = np.add(gamma, tmp_g)
+            tmp_xi = post.get_xi(alpha, beta, mfcc, t, transition, mean_guess,
+                                 covariance_guess)
+            tmp_gamma = post.get_gamma(alpha, beta, t)
+            xi = np.add(xi, tmp_xi)
+            gamma = np.add(gamma, tmp_gamma)
         new_transition = build_transition(xi, gamma)
         tmp = get_new_likelihood(ell, mfccs, new_transition, mean_guess,
                                  covariance_guess, epoch)
@@ -149,83 +150,126 @@ def train_transition(alphas, betas, mfccs, mean_guess, covariance_guess,
     return transition
 
 
-# This function trains the mean parameter.
-# Requires -
-# alphas:             All alphas.
-# betas:              All betas.
-# mfccs:              All MFCC matrices for all utterances.
-# mean_guess:         Initialize mean values for training.
-# covariance_guess:   Fixed covariance values
-# transition_guess:   Fixed transition matrix
-def train_mean(alphas, betas, mfccs, mean_guess, covariance_guess,
-               transition_guess):
+def train_gaussian(alphas, betas, mfccs, mean_guess,
+                   covariance_guess, transition):
     threshold = 1
+
     ell = len(alphas)
-    states = len(alphas[0][0])
     likelihood = [get_initial_likelihood(alphas)]
+    states = len(alphas[0][0])
     weighted_sum = list(np.multiply(mean_guess, 0))
+    weighted_covariance = list(np.multiply(mean_guess, 0))
     gamma_sum = [0] * states
+    # update mean over one epoch
     for epoch in range(0, ell):
         mfcc = mfccs[epoch]
+        mfcc_t = len(mfcc)
         alpha = alphas[epoch]
         beta = betas[epoch]
-        mfcc_t = len(mfcc)
-        for q in range(0, states):
-            for t in range(0, mfcc_t):
-                gamma = post.get_gamma(alpha, beta, t)
-                gamma_q = gamma[q]
-                tmp = np.multiply(mfcc[t], gamma_q)
-                weighted_sum[q] = list(np.add(tmp, weighted_sum[q]))
-                gamma_sum[q] += gamma_q
+        for t in range(0, mfcc_t):
+            gamma = post.get_gamma(alpha, beta, t)
+            for q in range(0, states):
+                add = np.multiply(mfcc[t], gamma[q])
+                weighted_sum = list(np.add(add, weighted_sum))
+                gamma_sum[q] += gamma[q]
         new_mean = list(np.divide(weighted_sum, gamma_sum))
-        tmp = get_new_likelihood(ell, mfccs, transition_guess, new_mean,
-                                 covariance_guess, epoch)
-        likelihood.append(tmp)
+
+        for t in range(0, mfcc_t):
+            gamma = post.get_gamma(alpha, beta, t)
+            for q in range(0, states):
+                variance = np.subtract(mfcc[t], new_mean[q])
+                variance = list(np.multiply(variance, variance))
+                add = np.multiply(variance, gamma[q])
+                weighted_covariance = list(np.add(add, weighted_covariance))
+        new_covariance = list(np.divide(weighted_covariance, gamma_sum))
+        new_like = get_new_likelihood(ell, mfccs, transition, new_mean,
+                                      new_covariance, epoch)
+        likelihood.append(new_like)
         improvement = likelihood_difference(likelihood)
         if improvement < threshold:
-            return new_mean
+            return new_mean, new_covariance
     new_mean = list(np.divide(weighted_sum, gamma_sum))
-    return new_mean
+    new_covariance = list(np.divide(weighted_covariance, gamma_sum))
+    return new_mean, new_covariance
 
 
-# This function trains the covariance parameter. This happens
-# separately from the function that trains the mean.
-# Requires -
-# alphas:             All alphas.
-# betas:              All betas.
-# mfccs:              All MFCC matrices for all utterances.
-# mean_guess:         Fixed mean values.
-# covariance_guess:   Initial covariance values for training.
-# transition_guess:   Fixed transition matrix
-def train_covariance(alphas, betas, mfccs, means, covariance_guess,
-                     transition_guess):
-    threshold = 1
-    ell = len(alphas)
-    states = len(alphas[0][0])
-    likelihood = [get_initial_likelihood(alphas)]
-    weighted_sum = list(np.multiply(covariance_guess, 0))
-    gamma_sum = 0
-    for epoch in range(0, ell):
-        alpha = alphas[epoch]
-        beta = betas[epoch]
-        mfcc = mfccs[epoch]
-        mfcc_t = len(mfcc)
-        for q in range(0, states):
-            mean_q = means[q]
-            for t in range(0, mfcc_t):
-                var = np.subtract(mfcc[t], mean_q)
-                var = list(np.multiply(var, var))
-                gamma = post.get_gamma(alpha, beta, t)
-                gamma_q = gamma[q]
-                gamma_sum += gamma_q
-                tmp = list(np.multiply(var, gamma_q))
-                weighted_sum[q] = list(np.add(weighted_sum[q], tmp))
-        new_covariance = list(np.divide(weighted_sum, gamma_sum))
-        tmp = get_new_likelihood(ell, mfccs, transition_guess, means,
-                                 new_covariance, epoch)
-        likelihood.append(tmp)
-        improvement = likelihood_difference(likelihood)
-        if improvement < threshold:
-            return new_covariance
-    new_covariance = list(np.divide(weighted_sum, gamma_sum))
-    return new_covariance
+# # This function trains the mean parameter.
+# # Requires -
+# # alphas:             All alphas.
+# # betas:              All betas.
+# # mfccs:              All MFCC matrices for all utterances.
+# # mean_guess:         Initialize mean values for training.
+# # covariance_guess:   Fixed covariance values
+# # transition_guess:   Fixed transition matrix
+# def train_mean(alphas, betas, mfccs, mean_guess, covariance_guess,
+#                transition_guess):
+#     threshold = 1
+#     ell = len(alphas)
+#     states = len(alphas[0][0])
+#     likelihood = [get_initial_likelihood(alphas)]
+#     weighted_sum = list(np.multiply(mean_guess, 0))
+#     gamma_sum = [0] * states
+#     for epoch in range(0, ell):
+#         mfcc = mfccs[epoch]
+#         alpha = alphas[epoch]
+#         beta = betas[epoch]
+#         mfcc_t = len(mfcc)
+#         for q in range(0, states):
+#             for t in range(0, mfcc_t):
+#                 gamma = post.get_gamma(alpha, beta, t)
+#                 gamma_q = gamma[q]
+#                 tmp = np.multiply(mfcc[t], gamma_q)
+#                 weighted_sum[q] = list(np.add(tmp, weighted_sum[q]))
+#                 gamma_sum[q] += gamma_q
+#         new_mean = list(np.divide(weighted_sum, gamma_sum))
+#         tmp = get_new_likelihood(ell, mfccs, transition_guess, new_mean,
+#                                  covariance_guess, epoch)
+#         likelihood.append(tmp)
+#         improvement = likelihood_difference(likelihood)
+#         if improvement < threshold:
+#             return new_mean
+#     new_mean = list(np.divide(weighted_sum, gamma_sum))
+#     return new_mean
+#
+#
+# # This function trains the covariance parameter. This happens
+# # separately from the function that trains the mean.
+# # Requires -
+# # alphas:             All alphas.
+# # betas:              All betas.
+# # mfccs:              All MFCC matrices for all utterances.
+# # mean_guess:         Fixed mean values.
+# # covariance_guess:   Initial covariance values for training.
+# # transition_guess:   Fixed transition matrix
+# def train_covariance(alphas, betas, mfccs, means, covariance_guess,
+#                      transition_guess):
+#     threshold = 1
+#     ell = len(alphas)
+#     states = len(alphas[0][0])
+#     likelihood = [get_initial_likelihood(alphas)]
+#     weighted_sum = list(np.multiply(covariance_guess, 0))
+#     gamma_sum = 0
+#     for epoch in range(0, ell):
+#         alpha = alphas[epoch]
+#         beta = betas[epoch]
+#         mfcc = mfccs[epoch]
+#         mfcc_t = len(mfcc)
+#         for q in range(0, states):
+#             mean_q = means[q]
+#             for t in range(0, mfcc_t):
+#                 var = np.subtract(mfcc[t], mean_q)
+#                 var = list(np.multiply(var, var))
+#                 gamma = post.get_gamma(alpha, beta, t)
+#                 gamma_q = gamma[q]
+#                 gamma_sum += gamma_q
+#                 tmp = list(np.multiply(var, gamma_q))
+#                 weighted_sum[q] = list(np.add(weighted_sum[q], tmp))
+#         new_covariance = list(np.divide(weighted_sum, gamma_sum))
+#         tmp = get_new_likelihood(ell, mfccs, transition_guess, means,
+#                                  new_covariance, epoch)
+#         likelihood.append(tmp)
+#         improvement = likelihood_difference(likelihood)
+#         if improvement < threshold:
+#             return new_covariance
+#     new_covariance = list(np.divide(weighted_sum, gamma_sum))
+#     return new_covariance
